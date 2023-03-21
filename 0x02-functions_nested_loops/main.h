84ce6beedb2e8e1119fca189a02f771365e68a8b
Your Name <you@example.com>
2023-03-21 08:45:49 -0700

    update_header

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include "stdio.h"
+
+void	_putchar(char c)
+{
+	putchar(c);
+}
+
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
