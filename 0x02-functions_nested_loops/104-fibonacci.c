a17767d8c4becdbe11019155cd11e37de80f69a2
Your Name <you@example.com>
2023-03-21 15:06:58 -0700

    98

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,23 +9,27 @@
 int main(void)
 {
 	long int f1;
+	int	i;
 	long int	f2;
 	long int	res;
-	long int	res2;
 
 	f1 = 1;
 	f2 = 2;
-	res = 0;
-	res2 = 2;
-	while (res < 4000000)
+	i  = 3;
+	res = f1;
+	printf("%ld, %ld, ", f1, f2);
+	while (i <= 98)
 	{
 		res = f2 + f1;
-		if (res % 2 == 0)
-			res2 += res;
+		if (i < 50)
+			printf("%ld, ", res);
+		else
+			printf("%ld", res);
 		f1 = f2;
 		f2 = res;
+		i++;
 	}
-	printf("%ld\n", res2);
+	printf("\n");
 	return (0);
 }
 
