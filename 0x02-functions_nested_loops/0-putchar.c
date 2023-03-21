f7e4c67b9a79d1e2eb6f9d5a1d286eed4291b825
Your Name <you@example.com>
2023-03-21 07:58:18 -0700

    puts

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 
 /*
- * main - print _putchar
+ * main- print _putchar
  *
  * Return: 0 (SUCCESS)
  */
 int main(void)
 {
-	printf("_putchar\n");
+	puts("_putchar");
 	return (0);
 }
 
