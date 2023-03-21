cdf634d3c47714a868b04f1738bc3e7b8805e724
Your Name <you@example.com>
2023-03-21 12:29:38 -0700

    @

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -5,8 +5,7 @@
  * print_times_table - print_times_table
  * @n: variable integer
  * Return: 0 ()
- * ft_test - function for fixe problem in betty style
- *
+ * @ft_test - function for fixe problem in betty style
  * @i: v i
  * @j: v i
  * @x: v i
