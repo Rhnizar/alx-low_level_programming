bbb824a86e407c12e904d21c19e089a25799789d
Your Name <you@example.com>
2023-03-22 14:02:40 -0700

    finish

--- 0x03-debuggin/positive_or_negative.c
+++ 0x03-debuggin/positive_or_negative.c
@@ -4,16 +4,16 @@
 
 /**
  * positive_or_negative- positive or negative number
- * @n: vriable integer 
+ * @i: vriable integer 
  * Return: 0
  */
-int positive_or_negative(int n)
+int positive_or_negative(int i)
 {
-	if (n == 0)
-		printf("%d is zero\n", n);
-	else if (n > 0)
-		printf("%d is positive\n", n);
-	else if (n < 0)
-		printf("%d is negative\n", n);
+	if (i == 0)
+		printf("%d is zero\n", i);
+	else if (i > 0)
+		printf("%d is positive\n", i);
+	else if (i < 0)
+		printf("%d is negative\n", i);
 	return (0);
 }
