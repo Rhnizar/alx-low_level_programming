3c79e169b4ce6cf39078ee2d235082d03d1c897b
Your Name <you@example.com>
2023-03-23 20:08:30 -0700

    hh

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,11 +2,11 @@
 #include "main.h"
 
 /**
- * _isdigit - print the alphabet 
+ * _isdigit - is digit
  * @c: v i
  * Retrun: 0 (SUCCESS)
  */
-int	_isdigit(int c)
+int _isdigit(int c)
 {
 	if (c >= '0' && c <= '9')
 		return (1);
