c1d25ad952d0ac09fb89c73689afadaaf04a739c
Your Name <you@example.com>
2023-03-23 20:00:29 -0700

    rrr

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,12 +3,11 @@
 /**
  * _isupper - is upper
  * @c: v i
- * Retrun: 0 (SUCCESS)
+ * Retrun: 0 || 1
  */
 int	_isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
 		return (1);
-	else
-		return (0);
+	return (0);
 }
