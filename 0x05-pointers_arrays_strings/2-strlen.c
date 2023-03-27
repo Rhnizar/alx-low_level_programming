98f2e75a22012692f12f97b8f093d19151629b13
Your Name <you@example.com>
2023-03-27 15:01:14 -0700

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/*
+ * _strlen - lenght string 
+ * @s: string
+ * Return: lenght of string 
+ */
+int _strlen(char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
