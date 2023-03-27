2f8b15804169cb0459b75669ae6bed8607a4f381
Your Name <you@example.com>
2023-03-27 14:52:57 -0700

    test

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * swap_int - swaps that values
+ * @a: pointer a
+ * @b: pointer b
+ */
+void swap_int(int *a, int *b)
+{
+	int	*tmp;
+	*tmp = *b;
+	*b = *a;
+	*a = *tmp;
+}
