aca380beb727c518f7472b70991cc7494dd065c4
Your Name <you@example.com>
2023-03-27 15:58:53 -0700

    test

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,6 +1,20 @@
 #include "main.h"
 
 /**
+ * ft_strlen - lenght string 
+ * @str: string
+ * Return: lenght string
+ */
+int	ft_strlen(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+/**
  * print_rev - print string in reverse
  * @s: string
  */
@@ -8,7 +22,7 @@ void print_rev(char *s)
 {
 	int	len;
 
-	len = _strlen(s);
-	while (len)
+	len = ft_strlen(s);
+	while (len != -1)
 		putchar(s[len--]);
 }
