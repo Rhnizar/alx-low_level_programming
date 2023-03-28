2b4db461975624efd94b2be92143cf47e8d9345b
Your Name <you@example.com>
2023-03-27 17:18:43 -0700

    pppp

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,35 +1,19 @@
 #include "main.h"
 
 /**
- * ft_strlen - lenght string
- * @str: string
- * Return: lenght string
- */
-int	ft_strlen(char *str)
-{
-	int i;
-
-	i = 0;
-	while (str[i])
-		i++;
-	return (i);
-}
-/**
  * puts2 - puts_2
  *@str: str
  */
 void puts2(char *str)
 {
 	int	i;
-	int	len;
 
 	i = 0;
-	len = ft_strlen(str) - 1;
-	while ((len / 2) != -1)
+	while (str[i])
 	{
-		putchar(str[i]);
-		i += 2;
-		len--;
+		if (i % 2 == 0)
+			putchar(str[i]);
+		i++;
 	}
 	putchar('\n');
 }
