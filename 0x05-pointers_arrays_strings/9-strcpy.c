84a5fe1cb36a35324bec14398b0abba8f8b81de8
Your Name <you@example.com>
2023-03-27 18:10:07 -0700

    stcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * ft_strlen - lenght string
+ * @str: string
+ * Return: lenght string
+ */
+int	ft_strlen(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+/**
+ * _strcpy - copy string
+ * @dest: pointer dest
+ * @src: pointer src
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int len;
+	int i;
+
+	len = ft_strlen(src);
+	i = 0;
+	dest = malloc(sizeof(char) * len + 1);
+	if (!dest)
+		return (NUll);
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+}
