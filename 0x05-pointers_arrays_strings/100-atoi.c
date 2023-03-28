ae7212516106f14b06b7b64121b5e84b99f4e806
Your Name <you@example.com>
2023-03-27 18:42:06 -0700

    atoi

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _atoi - array to integer
+ * @s: string
+ * Return: integer
+ */
+int _atoi(char *s)
+{
+	int i;
+	int res;
+	int is;
+	int signe;
+
+	i = 0;
+	res = 0;
+	is = 0;
+	signe = 1;
+	while (s[i])
+	{
+		if (s[i] == '-')
+			signe *= -1;
+		while (s[i] >= '0' && s[i] <= '9')
+		{
+			is = 1;
+			res *= 10 + s[i] - 48;
+			i++;
+		}
+		if (is == 1)
+			break;
+		i++;
+	}
+	return (res * signe);
+}
