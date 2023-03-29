bf19fdd826dc25d2c286f79e5243d0deff11cc62
Your Name <you@example.com>
2023-03-29 10:15:22 -0700

    test

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,12 +9,12 @@
 char *_strcat(char *dest, char *src)
 {
 	while (*dest != '\0')
-		dest++;
+		*dest++;
 	while (*src != '\0')
 	{
 		*dest = *src;
-		dest++;
-		src++;
+		*dest++;
+		*src++;
 	}
 	*dest = '\0';
 	return (dest);
