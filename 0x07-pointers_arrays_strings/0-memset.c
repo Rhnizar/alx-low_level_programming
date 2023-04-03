94d4fbac4ba85be79f0ad7cdb90257c4945a8421
Your Name <you@example.com>
2023-04-03 09:26:04 -0700

    ft_memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _memset - fills the first n bytes of the memory
+ * @s: pointer
+ * @b: char
+ * @n: integer
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int	i;
+
+	i = 0;
+	while (n-- > 0)
+		s[i++] = b;
+	return (s);
+}
