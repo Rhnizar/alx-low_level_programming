f1cdea4d3d5ecc09f9ace340c3487c81f17e3ab3
Your Name <you@example.com>
2023-04-03 09:16:30 -0700

    first commit in this task

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memcpy - function copies n bytes from memory area src
+ * @dest: pointer
+ * @src: pointer
+ * @n: integer
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int	i;
+
+	i = 0;
+	dest = malloc(sizeof(char) * n);
+	while (i <= n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
