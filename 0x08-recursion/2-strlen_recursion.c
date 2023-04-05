c15f34c3196d7a6643b5d35d0c34b0d73c49d12a
Your Name <you@example.com>
2023-04-05 10:28:09 -0700

    task 3

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - strlen recursion
+ * @s: string
+ */
+int _strlen_recursion(char *s)
+{
+	int res = 0;
+
+	if (*s)
+	{
+		res += _strlen_recursion (s + 1);
+		res++;
+	}
+	retrun (res);
+}
