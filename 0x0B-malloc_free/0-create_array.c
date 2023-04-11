bd457957dec869cf0fd191bbade7951f09156740
Your Name <you@example.com>
2023-04-11 13:27:32 -0700

    malloc && free

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -9,11 +9,16 @@
 
 char *create_array(unsigned int size, char c)
 {
-	char *a = malloc(size);
+	char *array;
+	unsigned int i;
 
 	if (size == 0)
 		return (NULL);
-	for (int i = 0 ; i < size ; i++)
-		a[size] = c;
+	array = (char *)malloc(sizeof(char) * size);
+	if (array == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		array[i] = c;
 	return (a);
 }
