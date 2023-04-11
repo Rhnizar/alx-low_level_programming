2471f3dc7beb450a1e09d5f976e1a0add67579d1
Your Name <you@example.com>
2023-04-11 13:30:58 -0700

    malloc && free

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,21 +9,35 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	int i, concat_i = 0, l = 0;
-	char *concat_s;
+	int i, j, len1, len2, len;
+	char *result;
 
-	if (s1 == NULL)
-		s1 = "";
-	else if (s2 == NULL)
-		s2 = "";
-	for (i = 0 ; s1[i] || s2[i] ; i++)
-		l++;
-	concat_s = malloc(sizeof(char) * l);
-	if (concat_s == NULL)
+	len1 = len2 = 0;
+
+	if (s1 != NULL)
+	{
+		i = 0;
+		while (s1[i++] != '\0')
+			len1++;
+	}
+
+	if (s2 != NULL)
+	{
+		i = 0;
+		while (s2[i++] != '\0')
+			len2++;
+	}
+
+	len = len1 + len2;
+	result = (char *)malloc(sizeof(char) * (len + 1));
+	if (result == NULL)
 		return (NULL);
-	for (i = 0 ; s1[i] ; i++)
-		concat_s[concat_i++] = s1[i];
-	for (i = 0 ; s2[i] ; i++)
-		concat_s[concat_i++] = s2[i];
-	return (concat_s);
+
+	for (i = 0; i < len1; i++)
+		result[i] = s1[i];
+	for (j = 0; j < len2; j++, i++)
+		result[i] = s2[j];
+	result[len] = '\0';
+
+	return (result);
 }
