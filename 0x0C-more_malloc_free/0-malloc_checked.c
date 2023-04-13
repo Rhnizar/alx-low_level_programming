263b2162041cadd533ea6441d2364f3dfe0a84dd
Your Name <you@example.com>
2023-04-13 10:41:06 -0700

    task finish

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -2,12 +2,13 @@
 
 /**
  * malloc_checked - check malloc
- * @b u i
+ * @b: u i
  * Return: pointer to allocate
  */
+
 void *malloc_checked(unsigned int b)
 {
-	void *str;
+	void	*str;
 
 	str = malloc(b);
 	if (!str)
