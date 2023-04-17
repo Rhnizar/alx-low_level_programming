46ecbb984cd12e2b3507c114166b34497790c9e0
Your Name <you@example.com>
2023-04-17 10:15:31 -0700

    task1 u3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -3,7 +3,15 @@
 
 # include<stdio.h>
 
-struct dog {
+/**
+ * struct dog - dog struct
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ */
+
+struct dog
+{
 	char	*name;
 	float	age;
 	char	*owner;
