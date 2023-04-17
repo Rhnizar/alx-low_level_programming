a5cb2f90e09de58aeaa741ab3868b82e0c31aa5c
Your Name <you@example.com>
2023-04-17 10:43:45 -0700

    print dog

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,28 @@
+#include "dog.h"
+
+/**
+ * print_dog - print dog
+ * @d : struct
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d)
+	{
+		if (d->name)
+			printf("Name: %s\n", d->name);
+		else
+			printf("Name: nil\n");
+		if (d->age)
+                        printf("Age: %s\n", d->age);
+                else
+                        printf("Age : nil\n");
+		if (d->Owner)
+                        printf("Owner: %s\n", d->owner);
+                else
+                        printf("nil\n");
+
+	}
+	else
+		printf("nil\n");
+}
