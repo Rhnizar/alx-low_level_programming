d15f6c13542d61592a3ff995c7dacea8202ee9e7
Your Name <you@example.com>
2023-04-29 04:23:45 -0700

    lenght list

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,21 @@
+
+
+/**
+ * listint_len - len list
+ * @h : list
+ * Return: number of elements
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	int	len;
+
+	len = 0;
+	while (h)
+	{
+		len++;
+		h = h->next;
+	}
+	return (len);
+}
+
